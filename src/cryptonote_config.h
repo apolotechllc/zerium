b36a9d8773272acf495648e36d86c5e7c51c583e
Hugo Teijiz <hugo.teijiz@gmail.com>
2017-10-07 20:54:53 -0300

    Bloque genesis

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -148,24 +148,24 @@ namespace config
 
   uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 18;
   uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 19;
-  uint16_t const P2P_DEFAULT_PORT = 11117;
-  uint16_t const RPC_DEFAULT_PORT = 11118;
+  uint16_t const P2P_DEFAULT_PORT = 18082;
+  uint16_t const RPC_DEFAULT_PORT = 18083;
   boost::uuids::uuid const NETWORK_ID = { {
 	  0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
 	} }; // Bender's nightmare
-  std::string const GENESIS_TX = "113c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
-  uint32_t const GENESIS_NONCE = 10000;
+  std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
+  uint32_t const GENESIS_NONCE = 20000;
 
   namespace testnet
   {
 	uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 53;
 	uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 54;
-	uint16_t const P2P_DEFAULT_PORT = 21117;
-	uint16_t const RPC_DEFAULT_PORT = 21118;
+	uint16_t const P2P_DEFAULT_PORT = 28082;
+	uint16_t const RPC_DEFAULT_PORT = 28083;
 	boost::uuids::uuid const NETWORK_ID = { {
 		0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
 	  } }; // Bender's daydream
-	std::string const GENESIS_TX = "113c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
-	uint32_t const GENESIS_NONCE = 10001;
+	std::string const GENESIS_TX = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101168d0c4ca86fb55a4cf6a36d31431be1c53a3bd7411bb24e8832410289fa6f3b";
+	uint32_t const GENESIS_NONCE = 20001;
   }
 }
